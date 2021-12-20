b1e701b6becb2f3486b4dcd02c9e471b754b05e9
vchivu14 <chivu.vlad@yahoo.com>
2021-12-20 09:54:16 +0100

    Initial commit

---
+++ src/main/java/com/example/elect/ElectApplication.java
@@ -0,0 +1,13 @@
+package com.example.elect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/elect/ElectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.elect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
