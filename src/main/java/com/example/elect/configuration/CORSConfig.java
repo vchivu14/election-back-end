9aa822dd8967d7940cfdc34bb65f2a157d5166c8
vchivu14 <chivu.vlad@yahoo.com>
2021-12-20 20:47:39 +0100

    CORS

---
+++ src/main/java/com/example/elect/configuration/CORSConfig.java
@@ -0,0 +1,25 @@
+package com.example.elect.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CORSConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:5500",
+                        "http://127.0.0.1:5500",
+                        "https://exam-election.surge.sh")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+
+}
